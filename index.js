0b11f84fcff0db996bb51326abfa8474f31a4e60
wisdomchuks <75422550+wisdomchuks@users.noreply.github.com>
2020-12-03 06:13:38 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,75 @@
+for(let i = 0; i<= 10; i++){
+    //console.log("hello", i)
+    //console.log("\n")
+    //console.log('2 *', i, '=', i * 2)
+    console.log('2*', i, '=', i*2 )
+}
+for(let i = 0; i<= 10; i++){
+    //console.log("hello", i)
+    //console.log("\n")
+    //console.log('2 *', i, '=', i * 2)
+    console.log('4*', i, '=', i*4 )
+}
+//const username= prompt("what is your name")
+
+//const password= prompt("what is your password")
+
+//console.log("table of 5*5")
+
+//if(username=== 'nicolas' && password ==='999')
+{
+   // alert('welcome' + username)
+}
+for(let i = 0; i<= 10; i++){
+    //console.log("hello", i)
+    //console.log("\n")
+    //console.log('2 *', i, '=', i * 2)
+    console.log('5*', i, '=', i*5 )
+}
+for(let i = 0; i<= 10; i++){
+    //console.log("hello", i)
+    //console.log("\n")
+    //console.log('2 *', i, '=', i * 2)
+    console.log('6*', i, '=', i*6 )
+}
+
+for(let i = 0; i<= 10; i++){
+    //console.log("hello", i)
+    //console.log("\n")
+    //console.log('2 *', i, '=', i * 2)
+    console.log('7*', i, '=', i*7 )
+}
+for(let i = 0; i<= 10; i++){
+    //console.log("hello", i)
+    //console.log("\n")
+    //console.log('2 *', i, '=', i * 2)
+    console.log('8*', i, '=', i*8 )
+}
+
+for(let i = 0; i<= 10; i++){
+    //console.log("hello", i)
+    //console.log("\n")
+    //console.log('2 *', i, '=', i * 2)
+    console.log('9*', i, '=', i*9 )
+}
+
+for(let i = 0; i<= 10; i++){
+    //console.log("hello", i)
+    //console.log("\n")
+    //console.log('2 *', i, '=', i * 2)
+    console.log('10*', i, '=', i*10 )
+}
+const username = prompt('what is your name')
+
+const age = prompt("How old are you")
+
+console.log(username)
+
+if(age <= 100){
+
+if(username === 'Nicolas' && age <= 100){
+        alert('hello' + username + '\nyou are spend' + age * 365 +
+        'days on the earth' )
+}
+
+}
